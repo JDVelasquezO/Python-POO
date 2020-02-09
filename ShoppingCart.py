6c07bdae6f11117bd918b851d7395ce31c1eb5b2
JDVelasquezO <jdvela158@gmail.com>
2020-02-09 07:40:20 -0600

    Polimorfism

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -1,8 +1,3 @@
-class Book:
-    def __init__(self, name, edition):
-        self.name = name
-        self.edition = edition
-
 class User:
     def __init__(self, name):
         self.name = name
@@ -11,20 +6,42 @@ class User:
     def add_book(self, book):
         self.books.append(book)
 
+    def presentation(self):
+        return f"Estudiante {self.name}"
+
 class Student(User):
     def __init__(self, name, license):
         User.__init__(self, name)
         self.license = license
+        
+    def showBook(self):
+        print(f"Estudiante {self.name} - {self.license}")
+        counter = 1
+        for book in self.books:
+            print (f"{counter} {book.name} - Edicion {book.edition}")
+            counter = counter + 1
+        print("\n")
+    
+    def presentation(self):
+        return f"Estudiante {self.name} - {self.license}"
 
-u = Student("Daniel", "201800722")
+class Book:
+    def __init__(self, name, edition):
+        self.name = name
+        self.edition = edition
+
+s1 = Student("Daniel", "201800722")
+s2 = Student("Bryan", "201700523")
+
 b1 = Book('Java POO', 8)
 b2 = Book('Python desde 0', 3)
+b3 = Book('TYPESCRIPT', 2)
+b4 = Book('Node desde cero', 11)
 
-u.add_book(b1)
-u.add_book(b2)
+s1.add_book(b1)
+s1.add_book(b3)
+s2.add_book(b2)
+s2.add_book(b4)
 
-print(f"Los libros de {u.name} - {u.license} son: ")
-counter = 1
-for u_book in u.books:
-    print(f"{counter} Libro {u_book.name} - Edicion {u_book.edition}")
-    counter = counter + 1
+s1.showBook()
+s2.showBook()
